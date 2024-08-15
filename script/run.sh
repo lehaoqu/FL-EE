b5b6a70798dcda5a51673d4a83a95c9661231659
root <root@gpu19.buaanlsde.org>
2024-08-15 23:43:46 +0800

    ignore

--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,7 @@
-dts=cifar10
-md=cnn
+dts=cifar100-224-d03
+md=vit
 sr=0.5
 
-python ../main.py fedavg --suffix $1 --dataset $dts --model $md --sr $sr
-python ../main.py fedasync --suffix $1 --dataset $dts --model $md --sr $sr
+python ../main.py heterofl --suffix $1 --dataset $dts --model $md --sr $sr
+# python ../main.py fedavg --suffix $1 --dataset $dts --model $md --sr $sr
+# python ../main.py fedasync --suffix $1 --dataset $dts --model $md --sr $sr
