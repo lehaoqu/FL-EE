3cc213d9409578cbd7f732a82e8cca285c368ace
qlh <2775257495@qq.com>
2024-10-17 23:07:49 +0800

    fix svhn

--- script/run_svhn-pl.sh
+++ script/run_svhn-pl.sh
@@ -11,18 +11,16 @@ lr=5e-2
 
 ply=$4
 
-for ply in "${policies[@]}"
-do
-    echo "$ply" 
-    # python main.py ours2        $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs --is_latent True --kd_lr 1e-3 --g_lr 1e-2 --g_eta 5 --g_gamma 10 
-    # python main.py inclusivefl  $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-    python main.py exclusivefl  $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
-    python main.py depthfl      $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
-    python main.py scalefl      $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
-    python main.py eefl         $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
-    python main.py reefl        $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
-    
-done
+
+echo "$ply" 
+# python main.py ours2        $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs --is_latent True --kd_lr 1e-3 --g_lr 1e-2 --g_eta 5 --g_gamma 10 
+# python main.py inclusivefl  $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+python main.py exclusivefl  $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
+python main.py depthfl      $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
+python main.py scalefl      $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
+python main.py eefl         $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
+python main.py reefl        $4 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
+
 
 
 
