9fbac8b2f1138785dac2f529c13d51f92dc28cbe
qlh <2775257495@qq.com>
2024-10-20 12:07:09 +0800

    fix cifar script

--- script/run_cifar.sh
+++ script/run_cifar.sh
@@ -9,20 +9,26 @@ md=vit
 bs=32
 
 
-policies=(l2w)
+policies=(base boosted l2w)
 
-for ply in "${policies[@]}"
-do
-    echo "$ply" 
-    python main.py ours2        $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs --is_latent True --kd_lr 1e-3 --g_lr 1e-2 --g_eta 5 --g_gamma 10 
-    python main.py inclusivefl  $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-    python main.py reefl        $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-    python main.py depthfl      $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-    python main.py scalefl      $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-    python main.py exclusivefl  $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-    # python main.py heterofl   $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-    python main.py eefl         $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
-done
+# for ply in "${policies[@]}"
+# do
+#     echo "$ply" 
+#     # python main.py ours2        $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs --is_latent True --kd_lr 1e-3 --g_lr 1e-2 --g_eta 5 --g_gamma 10 
+#     # python main.py inclusivefl  $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+#     python main.py reefl        $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+#     python main.py depthfl      $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+#     python main.py scalefl      $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+#     python main.py exclusivefl  $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+#     # python main.py heterofl   $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+#     python main.py eefl         $ply --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+# done
 
 
+python main.py reefl        $5 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+python main.py depthfl      $5 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+python main.py scalefl      $5 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+python main.py exclusivefl  $5 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+# python main.py heterofl   $5 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
+python main.py eefl         $5 --suffix $1 --device $2 --dataset $3 --model $md --sr $sr --total_num $total_num --lr $4 --bs $bs
 
