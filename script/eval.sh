bf8856a2b17200b620353a7bdc481e0ffe81703b
qlh <2775257495@qq.com>
2024-10-30 15:47:48 +0800

    fix eval for bert

--- script/eval.sh
+++ script/eval.sh
@@ -1,9 +1,9 @@
 # dts=cifar100-224-d03
-md=vit
+# md=vit
 
-# md=bert
+md=bert
 
 vr=0.2
 
-python eval.py depthfl boosted --suffix $1 --device $2 --dataset $3 --model $md --valid_ratio $vr --if_mode all --cosine
+python eval.py depthfl boosted --suffix $1 --device $2 --dataset $3 --model $md --valid_ratio $vr --if_mode all
 # python eval.py depthfl l2w --suffix $1 --device $2 --dataset $dts --model $md --valid_ratio $vr --if_mode anytime
