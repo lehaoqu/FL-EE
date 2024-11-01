ddd46d386093674f7a012423b1d6bd4859a3cffb
Qwtdgh <2775257495@qq.com>
2024-11-01 17:00:23 +0800

    add darkflpg

--- script/run_cifar_darkflpgup.sh
+++ script/run_cifar_darkflpgup.sh
@@ -12,5 +12,5 @@ python main.py darkflpgup boosted --suffix exps/darkflpgup_5e-2_1101/$1_soft --k
 # change gap_kd_lambda to 0.7
 python main.py darkflpgup boosted --suffix exps/darkflpgup_5e-2_1101/$1_soft --kd_join last --device $2 --dataset cifar100-224-d03 --model vit --sr 0.1 --total_num 120 --lr 0.05 --s_epoches 2 --valid_gap 10 --rnd 500 --is_feature True --loss_type $1 --sw learn --sw_type soft --s_epoches 2 --gap_kd_lambda 0.7
 
-# change s_epoches to [2]:  test s_epoches increase may acc increase
+# change s_epoches to [10]:  test s_epoches increase may acc increase
 python main.py darkflpgup boosted --suffix exps/darkflpgup_5e-2_1101/$1_soft --kd_join last --device $2 --dataset cifar100-224-d03 --model vit --sr 0.1 --total_num 120 --lr 0.05 --s_epoches 2 --valid_gap 10 --rnd 500 --is_feature True --loss_type $1 --sw learn --sw_type soft --s_epoches 10
--- trainer/generator/generator.py
+++ trainer/generator/generator.py
@@ -63,7 +63,7 @@ class Generator_LATENT(BaseModule):
             self.hidden_dim, self.token_num, self.hidden_rs, self.n_class, self.noise_dim, self.n_diff = 1000, 128, 256, 2, 2, 2
         self.latent_dim = self.token_num * self.hidden_rs
         
-        input_dim = self.noise_dim * 3 if self.embedding else self.noise_dim + self.n_class
+        input_dim = self.noise_dim * 2 if self.embedding else self.noise_dim + self.n_class
         self.fc_configs = [input_dim, self.hidden_dim]
         self.init_loss_fn()
         self.build_network()
@@ -73,7 +73,7 @@ class Generator_LATENT(BaseModule):
         self.diversity_loss = DiversityLoss(metric='l1')
         
     def build_network(self):
-        self.embedding_layer_diff = nn.Embedding(self.n_diff, self.noise_dim)
+        # self.embedding_layer_diff = nn.Embedding(self.n_diff, self.noise_dim)
         if self.embedding:
             self.embedding_layer = nn.Embedding(self.n_class, self.noise_dim)
         
@@ -89,16 +89,16 @@ class Generator_LATENT(BaseModule):
         # == Representation Layer ==
         self.representation_layer = nn.Linear(self.fc_configs[-1], self.latent_dim)
         
-    def forward(self, diffs, labels, eps):
+    def forward(self, labels, eps):
         if isinstance(labels, tuple): labels = labels[0]
         batch_size = labels.shape[0]
         
-        diff_embedding = self.embedding_layer_diff(diffs)
+        # diff_embedding = self.embedding_layer_diff(diffs)
         if self.embedding:
             y_input = self.embedding_layer(labels)
         else:
             y_input = F.one_hot(labels, num_classes=self.n_class).float()
-        z = torch.cat((eps, y_input, diff_embedding), dim=1)
+        z = torch.cat((eps, y_input), dim=1)
         # z = torch.cat((eps, y_input), dim=1)
         
         # == FC Layers ==
