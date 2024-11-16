9851a3d0c96bd97566046166ca956141360815c8
Qwtdgh <2775257495@qq.com>
2024-11-16 10:18:33 +0800

    BASE

--- script/run_cifar_base_lora.sh
+++ script/run_cifar_base_lora.sh
@@ -5,14 +5,19 @@ total_num=100
 
 md=vit
 bs=32
-lr=0.005
+lr=0.05
 
 noniids=(0.1 1 1000)
 
+if [ "$3" == "base" ]; then
+        lr=0.01
+fi
+
+
 for noniid in "${noniids[@]}"
 do
-    python main.py inclusivefl      $3  --ft $2 --suffix $1/${2}_${3}/noniid$noniid --device $4 --dataset cifar100_noniid$noniid --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
     python main.py eefl             $3  --ft $2 --suffix $1/${2}_${3}/noniid$noniid --device $4 --dataset cifar100_noniid$noniid --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
+    python main.py inclusivefl      $3  --ft $2 --suffix $1/${2}_${3}/noniid$noniid --device $4 --dataset cifar100_noniid$noniid --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
     python main.py depthfl          $3  --ft $2 --suffix $1/${2}_${3}/noniid$noniid --device $4 --dataset cifar100_noniid$noniid --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
     python main.py darkflpg         $3  --ft $2 --suffix $1/${2}_${3}/noniid$noniid --device $4 --dataset cifar100_noniid$noniid --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
     python main.py scalefl          $3  --ft $2 --suffix $1/${2}_${3}/noniid$noniid --device $4 --dataset cifar100_noniid$noniid --model $md --sr $sr --total_num $total_num --lr $lr --bs $bs
