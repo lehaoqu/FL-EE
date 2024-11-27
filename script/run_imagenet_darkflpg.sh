1c0a876866549708812ef5c855c5188e6cc19e94
Qwtdgh <2775257495@qq.com>
2024-11-27 20:53:12 +0800

    .sh

---
+++ script/run_imagenet_darkflpg.sh
@@ -0,0 +1,5 @@
+set -ex
+python main.py darkflpg boosted --ft full --suffix exps/test/$1/imagenet/full_boosted_100 --device $2 --dataset imagenet --model vit --sr 0.1 --total_num 100 --lr 0.05 --bs 32 --noise 100
+python main.py darkflpg boosted --ft full --suffix exps/test/$1/imagenet/full_boosted_150 --device $2 --dataset imagenet --model vit --sr 0.1 --total_num 100 --lr 0.05 --bs 32 --noise 150
+python main.py darkflpg boosted --ft full --suffix exps/test/$1/imagenet/full_boosted_200 --device $2 --dataset imagenet --model vit --sr 0.1 --total_num 100 --lr 0.05 --bs 32 --noise 200
+python main.py darkflpg boosted --ft full --suffix exps/test/$1/imagenet/full_boosted_500 --device $2 --dataset imagenet --model vit --sr 0.1 --total_num 100 --lr 0.05 --bs 32 --noise 500
