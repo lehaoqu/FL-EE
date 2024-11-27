588b59391da29d611eb99d355e27af1236f17ed1
Qwtdgh <2775257495@qq.com>
2024-11-27 20:37:19 +0800

    fix BASE BUG

--- trainer/alg/darkflpg.py
+++ trainer/alg/darkflpg.py
@@ -442,14 +442,14 @@ class Server(BaseServer):
                 gap_kd_loss = 0.0
                 if 'kd' in self.args.loss_type:
                     if direction == 'sl':
-                        # s, t = s_feature, t_feature.detach()
-                        # dist_loss = self.kd_dist_ratio*self.dist_criterion(s, t)
-                        # angle_loss = self.kd_angle_ratio*self.angle_criterion(s, t)
-                        # dark_loss = self.kd_dark_ratio*self.dark_criterion(s, t)
-                        # gap_kd_loss = weight_t_exits[s_exit_idx]*(dist_loss + angle_loss + dark_loss) * s.shape[0]
+                        s, t = s_feature, t_feature.detach()
+                        dist_loss = self.kd_dist_ratio*self.dist_criterion(s, t)
+                        angle_loss = self.kd_angle_ratio*self.angle_criterion(s, t)
+                        dark_loss = self.kd_dark_ratio*self.dark_criterion(s, t)
+                        gap_kd_loss = weight_t_exits[s_exit_idx]*(dist_loss + angle_loss + dark_loss) * s.shape[0]
                         
-                        s, t = s_logits, t_logits.detach()
-                        gap_kd_loss = weight_t_exits[s_exit_idx]* self.kd_response_ratio*self.kd_criterion(s, t) * s.shape[0] 
+                        # s, t = s_logits, t_logits.detach()
+                        # gap_kd_loss = weight_t_exits[s_exit_idx]* self.kd_response_ratio*self.kd_criterion(s, t) * s.shape[0] 
                     else:   
                         s, t = s_logits, t_logits.detach()
                         gap_kd_loss = weight_t_exits[s_exit_idx]* self.kd_response_ratio*self.kd_criterion(s, t) * s.shape[0] 
